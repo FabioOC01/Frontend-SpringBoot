e2e305c5ea0c5e28a6d97faa2f5544a096403e8e
FabioOC01 <ochoacuencafabio@gmail.com>
2024-11-13 23:50:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ListClientesComponent from './components/ListClientesComponent'; // Asegúrate de que la ruta sea correcta
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import AddClienteComponent from './components/AddClienteComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <HeaderComponent />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={<ListClientesComponent />}></Route>
+            <Route  path='/clientes' element={<ListClientesComponent />}></Route>
+            <Route  path='/add-cliente' element={<AddClienteComponent />}></Route>
+            <Route  path='/edit-cliente/:id' element={<AddClienteComponent />}></Route>            
+          </Routes>
+        </div>
+        <FooterComponent/>
+      </BrowserRouter>
     </div>
   );
 }
